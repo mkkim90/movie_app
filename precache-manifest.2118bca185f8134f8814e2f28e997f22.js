cd9086ada13e75c77a85edb55b0a7e64ca6b8730
mkkim90 <mkzzang0928@gmail.com>
2020-01-19 00:14:55 +0900

    Updates

---
+++ precache-manifest.2118bca185f8134f8814e2f28e997f22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d28d341e0d5946eb8f0332f0f61dd5c",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "f381314acb83a9d1d54e",
+    "url": "/movie_app/static/css/main.9f44dbc8.chunk.css"
+  },
+  {
+    "revision": "42a5b9873a447e8c5988",
+    "url": "/movie_app/static/js/2.4f74da51.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app/static/js/2.4f74da51.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f381314acb83a9d1d54e",
+    "url": "/movie_app/static/js/main.e77e311e.chunk.js"
+  },
+  {
+    "revision": "b03db121bc0725d553bd",
+    "url": "/movie_app/static/js/runtime-main.4970d62d.js"
+  }
+]);
